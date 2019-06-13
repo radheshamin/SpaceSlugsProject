aaa49ff78d04feadf88cc4f0e096a07c1da5e15e
radheshamin <radheshamin1999@gmail.com>
2019-06-12 23:56:12 -0400

    "started creating character screen"

--- app/src/main/java/edu/gatech/cs2340/spacetraders/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/ConfigurationActivity.java
@@ -1,4 +1,24 @@
 package edu.gatech.cs2340.spacetraders.views;
 
-public class ConfigurationActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.gatech.cs2340.spacetraders.R;
+import edu.gatech.cs2340.spacetraders.viewmodels.ConfigurationViewModel;
+
+/**
+ * This is the starting screen
+ */
+public class ConfigurationActivity extends AppCompatActivity {
+
+    /** local instance of viewmodel */
+    private ConfigurationViewModel configurationViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_character_activity);
+
+    }
+
 }
